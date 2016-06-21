ac892c1d3fb7c5131fdfc53f22ea1c26f19d10c3
Cassio Cardoso <caugusto.cardoso@gmail.com>
2016-06-20 22:02:31 -0300

    Add some mobile styling to posts, header, footer, about and contact components.

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+// App
+// var menuBtn = document.querySelector('.nav__toggle');
+
+function toggleMenu() {
+    var menuBtn = document.querySelector('.nav__toggle');
+
+    menuBtn.classList.add('is-visible');
+}
