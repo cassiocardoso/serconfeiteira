a5180b763714866d8a4f1a34dc62c5a4d0fa82ab
Cassio Cardoso <caugusto.cardoso@gmail.com>
2016-06-22 21:14:12 -0300

    Update menu styling for mobile devices.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,6 @@
 // var menuBtn = document.querySelector('.nav__toggle');
 
 function toggleMenu() {
-    var menuBtn = document.querySelector('.nav__toggle');
-
-    menuBtn.classList.add('is-visible');
+    var nav = document.querySelector('.nav__list');
+    nav.classList.toggle('is-visible');
 }
